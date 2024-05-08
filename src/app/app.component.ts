7147a55e6ca6fb52e46418cb1d0ca27225fabf25
sidmohanty11 <sidmohanty11@gmail.com>
2024-05-08 11:50:45 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+// fails because type imports cannot be injected
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { _processContentResult, fetchOneEntry } from '@builder.io/sdk-angular';
+
+interface BuilderProps {
+  apiVersion: string;
+  canTrack?: boolean;
+  trustedHosts?: undefined;
+  apiKey: string;
+  model: string;
+  content: any;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <content-variants
+      [model]="model"
+      [content]="content"
+      [apiKey]="apiKey"
+    ></content-variants>
+  `,
 })
 export class AppComponent {
-  title = 'angular-sdk-test-env';
+  title = 'angular';
+  apiKey: BuilderProps['apiKey'] = 'ad30f9a246614faaa6a03374f83554c9';
+  model: BuilderProps['model'] = 'page';
+  content: BuilderProps['content'];
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  async ngOnInit() {
+    // const urlPath = window.location.pathname || '';
+
+    const builderContent = await fetchOneEntry({
+      model: 'page',
+      apiKey: 'ad30f9a246614faaa6a03374f83554c9',
+      userAttributes: {
+        urlPath: '/angular-123',
+      },
+    });
+
+    if (!builderContent) {
+      return;
+    }
+
+    this.content = builderContent;
+
+    this.cdr.detectChanges();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Content } from '@builder.io/sdk-angular';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [Content, BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
