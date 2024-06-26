935bd594c22154c3d850f308a12efa9e71467a86
sidmohanty11 <sidmohanty11@gmail.com>
2024-06-26 15:30:14 +0530

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Content } from '@builder.io/sdk-angular';
-import { AppComponent } from './app.component';
+import { AppComponent, HelloComponent, TodoComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [Content, BrowserModule],
+  declarations: [AppComponent, TodoComponent, HelloComponent],
+  imports: [Content, BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
